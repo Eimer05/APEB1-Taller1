36a821b6aed4bf7f225008491796966d5199e8f9
Eimer Armijos <eaarmijos8@utpl.edu.ec>
2023-10-23 23:08:25 -0500

    Segundo commit

--- eaarmijos8-MiPrimerProyecto/proyecto1/src/proyecto1/Proyecto1.java
+++ eaarmijos8-MiPrimerProyecto/proyecto1/src/proyecto1/Proyecto1.java
@@ -14,6 +14,7 @@ public class Proyecto1 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        System.out.println("Hola mundo");
         // TODO code application logic here
     }
     
